fe10caa0b5f623d60fb764585a255ac7654dc5f2
Sinokholo <222705698@mycput.ac.za>
2025-03-28 14:11:08 +0200

    Created TestAppointmentFactory

---
+++ src/main/java/za/ac/cput/Factory/TestAppointmentFactory.java
@@ -0,0 +1,24 @@
+package za.ac.cput.Factory;
+
+import za.ac.cput.Domain.TestAppointment;
+
+import java.time.LocalDate;
+
+public class TestAppointmentFactory {
+    public static TestAppointment createTestAppointmentFactory(String testID, String testAddress, String testVenue, LocalDate testDate, int testAmount, String licenceCode, boolean testResult) {
+       //String testID = Helper.gernerateId();
+
+        if (testAddress == null || testVenue == null || testDate == null || licenceCode == null  ) {
+            return null;
+        }
+        return new TestAppointment.Builder()
+                .setTestID(testID)
+                .setTestAddress(testAddress)
+                .setTestVenue(testVenue)
+                .setTestDate(testDate)
+                .setTestResult(testResult)
+                .setLicenceCode(licenceCode)
+                .setTestAmount(testAmount)
+                .build();
+    }
+}
