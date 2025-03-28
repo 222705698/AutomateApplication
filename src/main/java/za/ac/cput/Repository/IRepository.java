2acdae26233b9adab1145128c31967d41e6f6664
admin <221807144@mycput.ac.za>
2025-03-28 14:41:01 +0200

    I created the Repository with the Interface

---
+++ src/main/java/za/ac/cput/Repository/IRepository.java
@@ -0,0 +1,12 @@
+package za.ac.cput.Repository;
+
+public interface IRepository<T,ID> {
+    T create(T t) ;
+    T read(ID id);
+    T update(T t);
+    boolean delete(ID id);
+// This is how abstraction is performed
+
+}
+
+
