6009f21f810fd386a8a6fe9cc35bae5363cc4b07
Robert Kings <tintothando9@gmail.com>
2025-03-28 13:39:44 +0200

    Added the Booking and Payments domain classes - 221482210

---
+++ src/main/java/za/ac/cput/Domain/Booking.java
@@ -0,0 +1,101 @@
+package za.ac.cput.Domain;
+
+import java.time.LocalDate;
+
+public class Booking {
+    private String bookingID;
+    private String bookingType;
+    private LocalDate bookingDate;
+    private double bookingAmount;
+    private String bookingDescription;
+    private String bookingStatus;
+
+    private Booking(BookBuilder bookBuilder){
+        this.bookingID = bookBuilder.bookingID;
+        this.bookingType = bookBuilder.bookingType;
+        this.bookingDate = bookBuilder.bookingDate;
+        this.bookingAmount = bookBuilder.bookingAmount;
+        this.bookingDescription = bookBuilder.bookingDescription;
+        this.bookingStatus = bookBuilder.bookingStatus;
+    }
+
+
+    public static class BookBuilder {
+        private String bookingID;
+        private String bookingType;
+        private LocalDate bookingDate;
+        private double bookingAmount;
+        private String bookingDescription;
+        private String bookingStatus;
+
+        public BookBuilder setBookingID(String bookingID) {
+            this.bookingID = bookingID;
+            return this;
+        }
+
+        public BookBuilder setBookingType(String bookingType) {
+            this.bookingType = bookingType;
+            return this;
+        }
+
+        public BookBuilder setBookingDate(LocalDate bookingDate) {
+            this.bookingDate = bookingDate;
+            return this;
+        }
+
+        public BookBuilder setBookingAmount(double bookingAmount) {
+            this.bookingAmount = bookingAmount;
+            return this;
+        }
+
+        public BookBuilder setBookingDescription(String bookingDescription) {
+            this.bookingDescription = bookingDescription;
+            return this;
+        }
+
+        public BookBuilder setBookingStatus(String bookingStatus) {
+            this.bookingStatus = bookingStatus;
+            return this;
+        }
+
+        public Booking build(){
+            return new Booking(this);
+        }
+    }
+
+    public String getBookingID() {
+        return bookingID;
+    }
+
+    public String getBookingType() {
+        return bookingType;
+    }
+
+    public LocalDate getBookingDate() {
+        return bookingDate;
+    }
+
+    public double getBookingAmount() {
+        return bookingAmount;
+    }
+
+    public String getBookingDescription() {
+        return bookingDescription;
+    }
+
+    public String getBookingStatus() {
+        return bookingStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "bookingID='" + bookingID + '\'' +
+                ", bookingType='" + bookingType + '\'' +
+                ", bookingDate=" + bookingDate +
+                ", bookingAmount='" + bookingAmount + '\'' +
+                ", bookingDescription='" + bookingDescription + '\'' +
+                ", bookingStatus='" + bookingStatus + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/za/ac/cput/Domain/Payments.java
@@ -0,0 +1,102 @@
+package za.ac.cput.Domain;
+
+import java.time.LocalDate;
+
+public class Payments {
+    private String paymentID;
+    private String paymentType;
+    private LocalDate paymentDate;
+    private String paymentMethod;
+    private double paymentAmount;
+    private String paymentDescription;
+
+    public Payments(PaymentBuilder paymentBuilder) {
+        this.paymentID = paymentBuilder.paymentID;
+        this.paymentType = paymentBuilder.paymentType;
+        this.paymentDate = paymentBuilder.paymentDate;
+        this.paymentMethod = paymentBuilder.paymentMethod;
+        this.paymentAmount = paymentBuilder.paymentAmount;
+        this.paymentDescription = paymentBuilder.paymentDescription;
+    }
+
+    public static class PaymentBuilder {
+        private String paymentID;
+        private String paymentType;
+        private LocalDate paymentDate;
+        private String paymentMethod;
+        private double paymentAmount;
+        private String paymentDescription;
+
+        public PaymentBuilder setPaymentID(String paymentID) {
+            this.paymentID = paymentID;
+            return this;
+        }
+
+        public PaymentBuilder setPaymentType(String paymentType) {
+            this.paymentType = paymentType;
+            return this;
+        }
+
+        public PaymentBuilder setPaymentDate(LocalDate paymentDate) {
+            this.paymentDate = paymentDate;
+            return this;
+        }
+
+        public PaymentBuilder setPaymentMethod(String paymentMethod) {
+            this.paymentMethod = paymentMethod;
+            return this;
+        }
+
+        public PaymentBuilder setPaymentAmount(double paymentAmount) {
+            this.paymentAmount = paymentAmount;
+            return this;
+        }
+
+        public PaymentBuilder setPaymentDescription(String paymentDescription) {
+            this.paymentDescription = paymentDescription;
+            return this;
+        }
+
+        public Payments build() {
+            return new Payments(this);
+        }
+    }
+
+    public String getPaymentID() {
+        return paymentID;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public LocalDate getPaymentDate() {
+        return paymentDate;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public double getPaymentAmount() {
+        return paymentAmount;
+    }
+
+    public String getPaymentDescription() {
+        return paymentDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "Payments{" +
+                "paymentID='" + paymentID + '\'' +
+                ", paymentType='" + paymentType + '\'' +
+                ", paymentDate=" + paymentDate +
+                ", paymentMethod='" + paymentMethod + '\'' +
+                ", paymentAmount=" + paymentAmount +
+                ", paymentDescription='" + paymentDescription + '\'' +
+                '}';
+    }
+
+
+}
