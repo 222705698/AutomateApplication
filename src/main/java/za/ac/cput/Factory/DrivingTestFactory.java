60def173643ff78a66c41ba8c4a71dc133c398a2
Sinokholo <222705698@mycput.ac.za>
2025-03-28 14:09:42 +0200

    Created DrivingTestFactory

---
+++ src/main/java/za/ac/cput/Factory/DrivingTestFactory.java
@@ -0,0 +1,19 @@
+package za.ac.cput.Factory;
+
+import za.ac.cput.Domain.DrivingTest;
+import za.ac.cput.Domain.TestAppointment;
+import za.ac.cput.Util.Helper;
+
+public class DrivingTestFactory {
+    public static DrivingTest createDrivingTestFactory(String examiner, TestAppointment test) {
+        String driverLicenseID = Helper.gernerateId();
+        if (Helper.isNullOrEmpty(examiner))
+            return null;
+
+        return new DrivingTest.Builder()
+                .setDriverLicenseID(driverLicenseID)
+                .setExaminer(examiner)
+                .setTest(test)
+                .build();
+    }
+}
